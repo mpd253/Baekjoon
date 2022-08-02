cc180422367be3128babb4d4ac374cbb654fb082
ChoiYoungKyu <82489360+mpd253@users.noreply.github.com>
2022-08-02 11:39:55 +0900

    [Bronze V] Title: 학점계산, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,17 @@
+str = input()
+
+if str=='A+': print(4.3) 
+if str == 'A0': print(4.0) 
+if str == 'A-': print(3.7)
+if str == 'B+': print(3.3)
+if str == 'B0': print(3.0) 
+if str == 'B-': print(2.7)
+if str == 'C+': print(2.3) 
+if str == 'C0': print(2.0) 
+if str == 'C-': print(1.7)
+if str == 'D+': print(1.3) 
+if str == 'D0': print(1.0) 
+if str == 'D-': print(0.7)
+if str == 'F': print(0.0)
+else:
+    pass
